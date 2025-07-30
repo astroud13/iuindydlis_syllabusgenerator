46fcb786ce62e66e8fd3fc787517bcee297e5b85
astroud13 <113217155+astroud13@users.noreply.github.com>
2025-07-30 10:39:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+function addOutcomeRow() {
+  alert('Outcome row logic will be added here.');
+}
+function addAssessmentRow() {
+  alert('Assessment row logic will be added here.');
+}
+function addScheduleRow() {
+  alert('Schedule row logic will be added here.');
+}
+function generatePreview() {
+  alert('Preview generation will be added here.');
+}
+function downloadDoc() {
+  alert('DOCX export will be added here.');
+}
